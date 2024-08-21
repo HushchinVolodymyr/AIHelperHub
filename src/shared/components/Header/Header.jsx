8aadc6c817c675a4b96ac86278b85d5a8aa18790
HushchinVolodymyr <volodymyrhushchin@gmail.com>
2024-08-22 01:29:37 +0300

    Header burger menu add

--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -1,13 +1,44 @@
-﻿import React from 'react';
+﻿'use client'
+import React, {useState} from 'react';
 import styles from './Header.module.css';
 import Link from "next/link";
 
+import {CiMenuBurger} from "react-icons/ci";
+import {IoMdClose} from "react-icons/io";
+
 const Header = () => {
+    const [burgerMenu, setBurgerMenu] = useState(false);
+
+    const burgerMenuSwitch = () => {
+        setBurgerMenu(!burgerMenu);
+    }
+
     return (
-        <header className={styles.container}>
-            <Link href="/" className={styles.link}>Home</Link>
-            <Link href="/" className={styles.link}>About</Link>
-            <Link href="/" className={styles.link}>Contact</Link>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <Link href="/" className={styles.link}>Home</Link>
+                <Link href="/" className={styles.link}>About</Link>
+                <Link href="/" className={styles.link}>Contact</Link>
+            </div>
+            {!burgerMenu ?
+                <CiMenuBurger
+                    className={styles.iconOpen}
+                    onClick={burgerMenuSwitch}
+                />
+                :
+                <div className={styles.burgerMenu}>
+                    <IoMdClose
+                        className={styles.iconClose}
+                        onClick={burgerMenuSwitch}
+                    />
+                    <div style={{display: "flex", flexDirection: 'column', marginTop: 10, gap: 10}}>
+                        <Link href="/" className={styles.link}>Home</Link>
+                        <Link href="/" className={styles.link}>About</Link>
+                        <Link href="/" className={styles.link}>Contact</Link>
+                    </div>
+                </div>
+            }
+
         </header>
     );
 };
