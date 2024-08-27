bf66b43689d986bc472285c75608ccd1fce36d62
HushchinVolodymyr <volodymyrhushchin@gmail.com>
2024-08-27 23:52:45 +0300

    Optimization img (2)

--- src/shared/components/UI/UICard/UICard.jsx
+++ src/shared/components/UI/UICard/UICard.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const UICard = (item) => {
     return (
         <div className={styles.card}>
-            <Image src={item.props.img} alt='card' width={300} height={300} className={styles.img} priority='height'/>
+            <Image src={item.props.img} alt='card' width={300} height={300} className={styles.img}/>
             <div className={styles.overlay}/>
             <div className={styles.textContainer}>
                 <h1 className={styles.mainText}>{item.props.text}</h1>
